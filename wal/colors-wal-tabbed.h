a511645f3c6a9e8e1849e3727283bc8b6284bc06
misserrorsomuch <121064875+misserrorsomuch@users.noreply.github.com>
2025-08-05 23:05:08 -0700

    Delete wal directory

--- wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#070e10";
-static const char* selfgcolor   = "#c1c2c3";
-static const char* normbgcolor  = "#06717e";
-static const char* normfgcolor  = "#c1c2c3";
-static const char* urgbgcolor   = "#053b78";
-static const char* urgfgcolor   = "#c1c2c3";
