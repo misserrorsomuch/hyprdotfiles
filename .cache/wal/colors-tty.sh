23415baaf5ee33c54251ec8205e579afb132837f
misserrorsomuch <121064875+misserrorsomuch@users.noreply.github.com>
2025-08-05 23:05:36 -0700

    Add files via upload

---
+++ .cache/wal/colors-tty.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+[ "${TERM:-none}" = "linux" ] && \
+    printf '%b' '\e]P0070e10
+                 \e]P1053b78
+                 \e]P206717e
+                 \e]P3166dba
+                 \e]P44374c7
+                 \e]P5149b92
+                 \e]P652a7be
+                 \e]P7c1c2c3
+                 \e]P8566669
+                 \e]P9053b78
+                 \e]PA06717e
+                 \e]PB166dba
+                 \e]PC4374c7
+                 \e]PD149b92
+                 \e]PE52a7be
+                 \e]PFc1c2c3
+                 \ec'
